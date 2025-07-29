6eb5ba2977b0eced1f561233a7db545b93080ce1
Anand <anand.work2005@gmail.com>
2025-07-29 08:19:29 +0530

    how to use useParams from react-router-dom

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,8 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import App from './App';
 import Home from './components/Home/Home'
 import About from './components/About/About';
+import Contact from './components/Contact/Contact';
+import User from './components/User/User';
 
 const router = createBrowserRouter([
   {
@@ -18,6 +20,14 @@ const router = createBrowserRouter([
       {
         path: '/about',
         element: <About />
+      },
+      {
+        path: '/contact',
+        element: <Contact />
+      },
+      {
+        path: '/user/:userid',
+        element: <User />
       }
     ]
   }
