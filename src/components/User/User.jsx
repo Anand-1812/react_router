1047e724ba6c3d9b3c4a94b3a513435ec537c274
Anand <anand.work2005@gmail.com>
2025-07-29 08:26:29 +0530

    add the user route and styled it

--- src/components/User/User.jsx
+++ src/components/User/User.jsx
@@ -4,7 +4,7 @@ import { useParams } from "react-router-dom";
 export default function User() {
   const { userid } = useParams();
   return (
-    <div>User: {userid}</div>
+    <div className="bg-gray-400 text-white text-3xl p-4 text-center">User: {userid}</div>
   );
 }
 
